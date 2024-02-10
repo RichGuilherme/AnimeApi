845cca70a03b4c84605f52fe6578fd2c05030b8f
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-10 11:36:46 -0300

    refactor: ajust no then e catch

--- src/dataBase/db.ts
+++ src/dataBase/db.ts
@@ -5,9 +5,9 @@ const connectDb = () => {
     mongoose.connect(process.env.MONGODB_URI || "", {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    } as ConnectOptions).
-        then(() => console.log("banco de dados conectado")).
-        catch((erro) => console.error("erro"))
+    } as ConnectOptions)
+    .then(() => console.log("banco de dados conectado"))
+    .catch((erro) => console.error("erro"))
 }
 
 export default connectDb
